a302be6a4edf11e2cbb4e1c2f8eb6c716ca9acb5
elpescador-nl <mark@elpescador.nl>
2017-01-24 15:49:46 +0100

    send SIGTERM signal to both emulators and ports

--- exitemulator.py
+++ exitemulator.py
@@ -1,6 +1,7 @@
 from time import sleep
 import signal
 import os
+import re
 import RPi.GPIO as GPIO
 
 gpio_pin_number=25
@@ -9,13 +10,14 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(gpio_pin_number, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 def exitEmulator(channel):
-    print('exitEmulator')
     pids = [pid for pid in os.listdir('/proc') if pid.isdigit()]
+
+    re_path = re.compile('\/opt\/retropie\/(emulators|ports)\/')
 
     for pid in pids:
         try:
             commandpath = open(os.path.join('/proc', pid, 'cmdline'), 'rb').read()
-            if commandpath[0:24] == '/opt/retropie/emulators/':
+            if re_path.match(commandpath):
                 os.kill(int(pid), signal.SIGTERM)
                 print('SIGTERM sent to process %s' % pid)
         except IOError:
